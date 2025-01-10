1d944442a58fd54e0cbb06e4f841861b9e8c0b22
towase <48572568+towase@users.noreply.github.com>
2025-01-10 22:24:08 +0900

    install script for Homebrew packages

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+packages=(
+  act
+  awscli
+  ffmpeg
+  gh
+  ghq
+  imagemagick
+  jq
+  k1low/tap/runn
+  lsd
+  neovim
+  suzuki-shunsuke/pinact/pinact
+  tfenv
+  tree
+  trivy
+  goodwithtech/r/dockle
+)
+
+for package in "${packages[@]}"; do
+  brew install "$package"
+done
