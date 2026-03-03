c839a937b67146b88002208f6a1478c2fbb83c12
stomy13 <48572568+stomy13@users.noreply.github.com>
2026-03-03 14:48:20 +0900

    feat: configure Dock settings for rows and columns in setup script

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ if [ -f ~/src/github.com/stomy13/dotfiles/alias.sh ]; then
     source ~/src/github.com/stomy13/dotfiles/alias.sh
 fi
 ' >> ~/.zshrc
+
+# Set the number of rows and columns for the Dock
+defaults write com.apple.dock springboard-rows -int 7
+defaults write com.apple.dock springboard-columns -int 8
+killall Dock
