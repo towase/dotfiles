0a5d2904827210e7dca76e048cd999a388bb63ad
towase <48572568+towase@users.noreply.github.com>
2026-03-27 12:54:07 +0900

    feat: enhance setup script with additional Dock settings and window management options

--- setup.sh
+++ setup.sh
@@ -6,12 +6,13 @@ ln -sf ~/src/github.com/towase/dotfiles/.gitconfig ~/.gitconfig
 
 ln -sf ~/src/github.com/towase/dotfiles/.zshrc ~/.zshrc
 
-# Set the number of rows and columns for the Dock
+# Dockの行数と列数を設定する
 defaults write com.apple.dock springboard-rows -int 7
 defaults write com.apple.dock springboard-columns -int 8
 
+# Dockを自動的に表示・非表示にする
 defaults write com.apple.dock autohide -bool true
+# デスクトップをクリックしてもウィンドウを表示しない
 defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
-defaults write com.apple.dock persistent-apps -array ""
 
 killall Dock
