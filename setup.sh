c8b1a91b215cbbafac92dd73ccbbae9f35bafe95
towase <48572568+towase@users.noreply.github.com>
2026-03-27 12:29:07 +0900

    refactor: streamline setup script by removing alias handling and linking .zshrc directly

--- setup.sh
+++ setup.sh
@@ -2,18 +2,7 @@
 
 set -euo pipefail
 
-alias_block='if [ -f ~/src/github.com/towase/dotfiles/alias.sh ]; then
-    source ~/src/github.com/towase/dotfiles/alias.sh
-fi'
-
-touch ~/.zshrc
-
-if ! grep -Fq "source ~/src/github.com/towase/dotfiles/alias.sh" ~/.zshrc; then
-  {
-    echo ""
-    echo "${alias_block}"
-  } >> ~/.zshrc
-fi
+ln -sf ~/src/github.com/towase/dotfiles/.zshrc ~/.zshrc
 
 # Set the number of rows and columns for the Dock
 defaults write com.apple.dock springboard-rows -int 7
